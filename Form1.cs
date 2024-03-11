738a80a7673b66fb7fdef337f1651922f75e5cd3
huf <hufgjb226@gmail.com>
2024-03-11 15:05:56 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+            private void btnGo_Click(object sender, EventArgs e)
+            {
+                txtShow.Text = "Hello World!"; //你可以直接複製貼過去
+            }
+        
+    }
+}
